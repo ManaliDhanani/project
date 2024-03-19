6189a9387df1145d57335d0b4e8fa123e240315b
ManaliDhanani <manalidhanani105@gmail.com>
2024-03-19 15:28:43 +0530

    change

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -3,13 +3,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     exports: [
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
-        MatListModule
+        MatListModule,
+        MatSliderModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule
     ]
 })
 
