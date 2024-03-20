f4dc89a1bf2a6aa00c860b9853597c4f0c5fbc06
ManaliDhanani <manalidhanani105@gmail.com>
2024-03-20 15:03:31 +0530

    change

--- src/app/services/api-interceptor.service.ts
+++ src/app/services/api-interceptor.service.ts
@@ -5,9 +5,7 @@ export class ApiInterceptor implements HttpInterceptor {
     private apiUrl: string = environment.hosts[0].target;
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const modifiedReq = req.clone({
-            url: `${this.apiUrl}${req.url}` 
-        });
+        const modifiedReq = req.clone({ url: `${this.apiUrl}${req.url}` });
         return next.handle(modifiedReq);
     }
 }
